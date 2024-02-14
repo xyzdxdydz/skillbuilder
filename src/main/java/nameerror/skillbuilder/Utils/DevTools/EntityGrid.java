ad6282e061da8e762dbfbbfb4b7e52d2c419371e
xyzdxdydz <mastermind2012@hotmail.co.th>
2024-02-15 02:56:34 +0700

    [Add] entity grid tool

---
+++ src/main/java/nameerror/skillbuilder/Utils/DevTools/EntityGrid.java
@@ -0,0 +1,26 @@
+package nameerror.skillbuilder.Utils.DevTools;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Allay;
+import org.bukkit.util.Vector;
+
+public class EntityGrid {
+
+    public static void makeGrid(Location center, int size, int stride) {
+        if (stride < 1) {
+            stride = 1;
+        }
+        for (int x=-size; x<=size; x+=stride) {
+            for (int y=-size; y<=size; y+=stride) {
+                for (int z=-size; z<=size; z+=stride) {
+                    Location spawnLocation = center.clone().add(new Vector(x, y, z));
+                    Allay allay = center.getWorld().spawn(spawnLocation, Allay.class);
+
+//                    allay.setGravity(false);
+                    allay.setAI(false);
+//                    allay.setCollidable(false);
+                }
+            }
+        }
+    }
+}
