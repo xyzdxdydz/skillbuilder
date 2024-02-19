ed27766966424a1e2d05b020a7af01a24f6fda8a
xyzdxdydz <mastermind2012@hotmail.co.th>
2024-02-19 18:49:41 +0700

    [Update] Set space properties

--- src/main/java/nameerror/skillbuilder/Math/SetSpace.java
+++ src/main/java/nameerror/skillbuilder/Math/SetSpace.java
@@ -1,10 +1,32 @@
 package nameerror.skillbuilder.Math;
 
+import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 
 import java.util.ArrayList;
 
 public abstract class SetSpace {
-    public abstract ArrayList<Entity> findEntities(boolean useBoundingBox);
+    private Location location;
 
+    protected SetSpace(Location location) {
+        this.location = location;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public boolean setLocation(Location location) {
+        this.location = location;
+        return true;
+    }
+
+//    @Override
+//    public abstract ArrayList<Entity> itemInside(ArrayList<Location> locations);
+    // TODO; find by bounding box
+    // TODO; try to make it generic (if possible)
+
+    public abstract ArrayList<Entity> findEntities(boolean useBoundingBox);
+    public abstract ArrayList<Block> findBlocks(boolean useBoundingBox);
 }
--- src/main/java/nameerror/skillbuilder/Math/Shape/Sphere.java
+++ src/main/java/nameerror/skillbuilder/Math/Shape/Sphere.java
@@ -3,6 +3,7 @@ package nameerror.skillbuilder.Math.Shape;
 
 import nameerror.skillbuilder.Math.SetSpace;
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.util.Vector;
 
@@ -11,11 +12,10 @@ import java.util.Collection;
 import java.util.Set;
 
 public class Sphere extends SetSpace implements Shape3D, Shape {
-    private final Location location;
     private final double radius;
 
     public Sphere(Location location, double radius) {
-        this.location = location;
+        super(location);
         this.radius = radius;
     }
 
@@ -39,14 +39,41 @@ public class Sphere extends SetSpace implements Shape3D, Shape {
         double randomPhi = Math.random() * 180;
         double randomTheta = Math.random() * 360;
         Vector normal = new Vector(0, 0, 1);
-        return location.clone().add(new Vector(0, 1, 0).multiply(randomRange).rotateAroundAxis(normal, randomPhi)
+        return this.getLocation().clone().add(new Vector(0, 1, 0).multiply(randomRange).rotateAroundAxis(normal, randomPhi)
                 .rotateAroundAxis(new Vector(0, 1, 0), randomTheta));
     }
 
     @Override
     public ArrayList<Entity> findEntities(boolean useBoundingBox) {
-        Collection<Entity> entities = location.getWorld().getNearbyEntities(location, 2*radius, 2*radius, 2*radius);
-        entities.removeIf(e -> location.clone().distance(e.getLocation()) > radius);
+        Collection<Entity> entities = this.getLocation().getWorld().getNearbyEntities(this.getLocation(), 2*radius, 2*radius, 2*radius);
+        entities.removeIf(e -> this.getLocation().clone().distance(e.getLocation()) > radius);
         return (ArrayList<Entity>) entities;
     }
+
+    @Override
+    public ArrayList<Block> findBlocks(boolean useBoundingBox) {
+        ArrayList<Block> blockResult = new ArrayList<>();
+
+        for (double x =-radius; x<=radius; x++) {
+            for (double y=-radius; y<=radius; y++) {
+                for (double z=-radius; z<=radius; z++) {
+
+                    Vector locOffset = new Vector(x, y, z);
+                    if (locOffset.distanceSquared(new Vector(0, 0, 0)) <= radius*radius) {
+                        Location blockLoc = this.getLocation().clone().add(locOffset);
+                        blockResult.add(blockLoc.getBlock());
+                    }
+                }
+            }
+        }
+
+        return blockResult;
+    }
+
+    // make it to pair of T, location
+//    public ArrayList<Entity> itemInside(ArrayList<Location> locations) {
+//        Collection<Entity> entities = location.getWorld().getNearbyEntities(location, 2*radius, 2*radius, 2*radius);
+//        locations.removeIf(e -> this.getLocation().clone().distance(e) > radius);
+//        return (ArrayList<Entity>) entities;
+//    }
 }
