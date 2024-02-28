ea7e655cc61a8493196ffdf3313121d8e8cc4bf4
xyzdxdydz <mastermind2012@hotmail.co.th>
2024-02-28 20:07:10 +0700

    [Update] abstract AoEProjectile

--- src/main/java/nameerror/skillbuilder/Fundamental/ObjectManagement/AoEProjectile.java
+++ src/main/java/nameerror/skillbuilder/Fundamental/ObjectManagement/AoEProjectile.java
@@ -2,14 +2,8 @@ package nameerror.skillbuilder.Fundamental.ObjectManagement;
 
 import nameerror.skillbuilder.Math.SetSpace;
 import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
 
-import java.util.ArrayList;
-
-public class AoEProjectile extends SBProjectile {
+public abstract class AoEProjectile extends SBProjectile {
     private SetSpace setSpace;
 
     public AoEProjectile(SetSpace setSpace) {
@@ -17,34 +11,17 @@ public class AoEProjectile extends SBProjectile {
         this.setSpace = setSpace;
     }
 
+    public SetSpace getSetSpace() {
+        return this.setSpace;
+    }
+
+    public void setSetSpace(SetSpace setSpace) {
+        this.setSpace = setSpace;
+    }
 
     @Override
     public boolean teleport(Location location) {
         setSpace.setLocation(location);
         return super.teleport(location);
-    }
-
-    @Override
-    public void onLaunch() {
-
-    }
-
-    @Override
-    public void onHit() {
-
-    }
-
-    @Override
-    public void onFly() {
-        ArrayList<Block> blocks = setSpace.findBlocks(false);
-        ArrayList<Entity> entities = setSpace.findEntities(false);
-
-        blocks.forEach(block -> block.setType(Material.AIR));
-        entities.forEach(entity -> {
-            if (entity instanceof LivingEntity) {
-                ((LivingEntity) entity).damage(10);
-                ((LivingEntity) entity).setNoDamageTicks(0);
-            }
-        });
     }
 }
