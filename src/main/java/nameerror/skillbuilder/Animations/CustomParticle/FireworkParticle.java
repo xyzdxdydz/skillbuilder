3dd3979d1d33fbba1f90d834467aafae53d95eed
xyzdxdydz <mastermind2012@hotmail.co.th>
2024-02-29 12:23:48 +0700

    [Add] Firework Particle

---
+++ src/main/java/nameerror/skillbuilder/Animations/CustomParticle/FireworkParticle.java
@@ -0,0 +1,45 @@
+package nameerror.skillbuilder.Animations.CustomParticle;
+
+import org.bukkit.*;
+import org.bukkit.entity.Firework;
+import org.bukkit.inventory.meta.FireworkMeta;
+
+import java.lang.reflect.Field;
+
+public class FireworkParticle {
+    private FireworkEffect.Type fireworkType;
+    private Color color;
+    private boolean trail;
+    private boolean flicker;
+
+    public FireworkParticle(FireworkEffect.Type fireworkType, Color color, boolean trail, boolean flicker) {
+        this.fireworkType = fireworkType;
+        this.color = color;
+        this.trail = trail;
+        this.flicker = flicker;
+    }
+
+    public void spawn(Location location) {
+        World world  = location.getWorld();
+        FireworkEffect fwFx = FireworkEffect.builder().trail(trail).flicker(flicker).withColor(color).with(fireworkType).build();
+
+        final Firework fw = world.spawn(location, Firework.class);
+        FireworkMeta meta = fw.getFireworkMeta();
+        meta.addEffect(fwFx);
+
+        // this equivalent to
+        // meta.setPower(10);
+        Field power;
+        try {
+            power = meta.getClass().getDeclaredField("power");
+            power.setAccessible(true);
+            power.set(meta, 0);
+
+        } catch (IllegalAccessException | IllegalArgumentException | SecurityException | NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
+        fw.setFireworkMeta(meta);
+        fw.detonate();
+    }
+}
