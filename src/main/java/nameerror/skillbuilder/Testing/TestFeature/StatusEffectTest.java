d702e6b77af9c251110892df14f9cbf679321c3e
xyzdxdydz <mastermind2012@hotmail.co.th>
2024-02-29 23:20:40 +0700

    [Add] DoT Test

--- src/main/java/nameerror/skillbuilder/Testing/TestFeature/StatusEffectTest.java
+++ src/main/java/nameerror/skillbuilder/Testing/TestFeature/StatusEffectTest.java
@@ -1,9 +1,14 @@
 package nameerror.skillbuilder.Testing.TestFeature;
 
+import nameerror.skillbuilder.Fundamental.ObjectManagement.LegacyEntity;
+import nameerror.skillbuilder.Fundamental.StatusEffect.DoT;
+import nameerror.skillbuilder.Fundamental.StatusEffect.StatusEffectManager;
 import nameerror.skillbuilder.SkillBuilder;
 import nameerror.skillbuilder.Testing.TestModule;
 import nameerror.skillbuilder.Fundamental.StatusEffect.CrowControl.Snare;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Player;
 
 public class StatusEffectTest extends TestModule {
@@ -12,9 +17,10 @@ public class StatusEffectTest extends TestModule {
 
     private void initTest() {
         this.addTestCase("cc_snare", this::CCSnareTest);
+        this.addTestCase("DoT_basic", this::basicDoT);
     }
 
-    public Integer CCSnareTest(Player player) {
+    private Integer CCSnareTest(Player player) {
         Snare.addSnared(player);
         player.sendMessage("A 'snared' effect will be removed in next 10 seconds.");
         Bukkit.getScheduler().runTaskLater(SkillBuilder.getPlugin(), () -> {
@@ -24,4 +30,13 @@ public class StatusEffectTest extends TestModule {
         return 0;
     }
 
+    private Integer basicDoT(Player player) {
+        Location spawnLoc = player.getEyeLocation().add(player.getLocation().getDirection().multiply(5));
+        Creeper creeper = player.getWorld().spawn(spawnLoc, Creeper.class);
+        DoT dot = new DoT(LegacyEntity.get(player), LegacyEntity.get(creeper),
+                20*8,20,1,1, 2);
+
+        StatusEffectManager.applyEffect(creeper, dot);
+        return 0;
+    }
 }
